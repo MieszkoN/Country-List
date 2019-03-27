2af148bc45f2d97d7a11672751b4aa6c72ac6835
MieszkoN <mieszkoniewiarowski@wp.pl>
2019-03-27 22:42:23 +0100

    Add files via upload

--- countrylist.cpp
+++ countrylist.cpp
@@ -1,107 +1,110 @@
 #include <iostream>
-#include <string>
 #include <cstring>
 #include "countrylist.h"
-#include "country.h"
 
 using namespace std;
 
+//constructor
 CountryList::CountryList() {
 	head = NULL;
 }
 
+//destructor
 CountryList::~CountryList() {
 	Country *temp = head;
-	while(temp) {
-		temp = temp->next;
-		//temp = temp->getNext();
-		//temp->setNext(temp->getNext());
+	while (temp != NULL) {
+		temp = temp->getNext();
 		delete head;
 		head = temp;
 	}
+	cout << "Linked list is empty" << endl;
 }
 
-void CountryList::addCountry(int k, const char *n, long pop)
-{
-    Country *add = new Country(k, n, pop);
-    Country *temp = head;
-    add->setKey(k);
-    add->setName(n);
-    add->setPopulation(pop);
-    temp = head;
-    if(temp == NULL) {
+//copy constructor
+CountryList::CountryList(const CountryList &x) {
+	//Country *temp = x.getHead();
+	Country *temp = x.head;
+	Country *ptr = head;
+	
+	while(ptr) {
+		if(!ptr->getNext())
+			break;
+		ptr = ptr->getNext();
+	}
+
+	while(temp) {
+		Country *add = new Country(temp->getKey(), temp->getName(), temp->getPopulation());
+		add->setNext() = NULL;
+		
+		if(!head) {
+			head = add;
+		} else {
+			ptr->setNext() = add;
+		}
+		ptr = add;
+		temp = temp->getNext();
+	}
+}
+
+//add Country to the list of the Countries
+void CountryList::addCountry(int k, const char *n , long p) {
+	Country *add = new Country(k, n, p);
+	Country *temp = head;
+	if (head == NULL) {
 		head = add;
+		head->setNext() = NULL;
 		return;
 	}
 	
 	while (temp != NULL) {
-        if (temp->getKey() == k) {
-            cout << "There is an element with such key!" << endl;
-            return;
-        }
-        temp = temp->getNext();
-        //temp = temp->next;
-        //temp->setNext(temp->getNext());
-    }
-    Country *prev = NULL;
-	while((temp != NULL) && strcmp(temp->getName(), add->getName()) < 0) {
+		if ((temp->getKey()) == k) {
+			cout << "There is element with such key!" << endl;
+			return;
+		}
+		temp = temp->getNext();
+	}
+	temp = head;
+	Country *prev = NULL;
+	while ((temp != NULL) && strcmp(temp->getName(), add->getName()) <= 0) {
 		prev = temp;
 		temp = temp->getNext();
-		//temp = temp->next;
-		//temp->setNext(temp->getNext());
 	}
-	
-    if(prev != NULL) {
-		Country *temp1;
-		add->next = prev->next;
-		prev->next = add;
-		//temp1 = add->getNext();
-		//temp1 = prev->getNext();
-		//temp1 = add;
-		//prev->getNext() = add;
-		//add->getNext() = prev->getNext();
-		//prev->getNext() = add;
-		//add->setNext(prev->getNext());
-		//prev->setNext(add);
-	} else 	{
-		Country *temp2;
-		//temp2 = add->getNext();
-		add->next = head;
-		//add->getNext() = head;
-		//add->setNext(head);
-		head = add;
-	}
+		if (prev != NULL) {
+			add->setNext() = prev->getNext();
+			prev->setNext() = add;
+		} else {
+			add->setNext() = head;
+			head = add;
+		}
 }
 
+//remove country from the list of the countries
 void CountryList::removeCountry(int k) {
-    Country *temp = head;  
-    Country *prev = temp; 
-    if(head == NULL) {
-        cout << endl << "List is empty" << endl;
-    } else if (head->getKey() == k) {
-        head = temp->getNext();
-        delete temp;
-    } else  {
-        while (temp != NULL) {
-            if (temp->getKey() == k) {
-                if (temp->getNext() == NULL) {   // tu było 0
-                    prev->next = NULL;
-                    //prev->getNext() = NULL;      // tu było 0
-                    delete temp;
-                    return;
-                } else {
-					prev->next = temp->next;
-                    //prev->getNext() = temp->getNext();
-                    delete temp;
-                    return;
-                }
-            }
-            prev = temp;
-            temp = temp->getNext();
-        }
-    }
+	Country *temp = head;
+	Country *prev = temp;
+	if (head->getKey() == k) {
+		head = temp->getNext();
+		delete temp;
+	} else {
+		while (temp != NULL) {
+			if (temp->getKey() == k) {
+				if (temp->getNext() == NULL)  {
+					prev->setNext() = NULL;
+					delete temp;
+					return;
+				} else {
+					prev->setNext() = temp->getNext();
+					delete temp;
+					return;
+				}
+			}
+			prev = temp;
+			temp = temp->getNext();
+		}
+	}
 }
 
+//print all countries on the screen
 void CountryList::displayList() {
 	Country *ptr = head;
 	if(ptr == NULL) {
@@ -111,5 +114,14 @@ void CountryList::displayList() {
   			cout << ptr->getKey() << " " <<  ptr->getName() << " " << ptr->getPopulation() << endl;
 			ptr = ptr->getNext();
 		}
+	}
+}
+
+//assignment operator in order to copy linked list
+CountryList& CountryList::operator=(const CountryList& x) {
+	if(!head) {
+		return *this;
+	} else {
+		cout << "Linked list is empty!"<< endl;
 	}
 }
