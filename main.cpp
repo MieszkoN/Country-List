53f8b4ecf50ceac9c33234f5228d6f22029e73de
MieszkoN <mieszkoniewiarowski@wp.pl>
2019-03-27 22:42:31 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,35 +1,54 @@
 #include <iostream>
-#include <string>
 #include <cstring>
 #include "countrylist.h"
-#include "country.h"
 
 using namespace std;
 
-int main() {
-	Country *c1 = new Country(2, "Brazil", 207679000);
-	c1->print();
+int main()
+{
+	/*
+	CountryList list1;
+	list1.addCountry(6, "Belgium", 11303528);
+	list1.addCountry(2, "Uruguay", 3360148);
+	list1.addCountry(4, "Zimbabwe", 14877000);
+	list1.addCountry(1, "Poland", 38413000);
+	list1.addCountry(3, "Germany", 82887000);
+	list1.addCountry(5, "Argentina", 44082000);
+	list1.addCountry(3, "Sweden", 9995000); // there is element with such key
+	list1.displayList();
+	cout<<endl;
+    list1.removeCountry(5);
+    list1.removeCountry(4);
+	list1.displayList();
+	*/
+	cout << endl << endl << endl;
 	
+	
+	CountryList *list2 = new CountryList;
+	list2->displayList();  //no element in the list
+	list2->addCountry(6, "Belgium", 11303528);
+	list2->addCountry(2, "Uruguay", 3360148);
+	list2->addCountry(4, "Zimbabwe", 14877000);
+	list2->addCountry(1, "Poland", 38413000);
+	list2->addCountry(3, "Germany", 82887000);
+	list2->addCountry(5, "Argentina", 44082000);
+	list2->displayList();
 	cout << endl;
-	CountryList *list = new CountryList;
-	list->displayList();  //no element in the list
-	list->addCountry(2, "Uruguay", 3360148);
-	list->addCountry(4, "Zimbabwe", 14877000);
-	list->addCountry(1, "Poland", 38413000);
-	list->addCountry(3, "Germany", 82887000);
-	list->addCountry(5, "Argentina", 44082000);
-	list->displayList();
+	list2->addCountry(2, "Brazil", 207679000); //same key as Uruguay
+	list2->displayList();
+	list2->removeCountry(10); //no element with such a key
+	list2->removeCountry(5);
+	list2->removeCountry(3);
 	cout << endl;
-	list->addCountry(2, "Brazil", 207679000); //same key as Uruguay
-	list->displayList();
-	list->removeCountry(3);
-	list->removeCountry(2);
+	list2->displayList();
 	cout << endl;
-	list->displayList();
-	cout << endl;
-	list->removeCountry(6); //no element with such a key
-	delete list;
-	list->displayList();
-	delete c1;
+	
+	cout << endl << endl << "Copied linked list" << endl;
+	CountryList *l1 = list2;
+	l1->displayList();	
+	
+	delete list2;
+	
+	//l1->displayList();
 	return 0;
 }
